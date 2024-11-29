974fd34048540a20e307c70f79bcdea3ff8c8935
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-11-29 17:30:23 +0200

    Installed Heroicons library for UI icons. Implemented toggle logic for menu open/close functionality.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,53 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+
 import styles from './Header.module.css';
 
 const Header = ({ logo, menuItems }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onToggle = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <header className={styles.header}>
             <div className={styles.container}>
+                {/* Лого */}
                 <div className={styles.logo}>
                     <a href="/">
-                        <img src={logo} alt="Logo" className={styles.logo} />
+                        <img src={logo} alt="Logo" className={styles.logoImg} />
                     </a>
                 </div>
+
+                {/* Навигационно меню */}
                 <nav className={styles.navContainer}>
-                    <ul className={styles.navList}>
-                        {menuItems
-                            .filter(item => item.url)
-                            .map((item, index) => (
-                                <li key={index} className={styles.navItem}>
-                                    <a href={item.url} className={styles.navLink}>
-                                        {item.name}
-                                    </a>
-                                </li>
-                            ))}
+                    {/* Бутон за отваряне/затваряне */}
+                    <button className={styles.menuToggle} onClick={onToggle}>
+                        {isOpen ? (
+                            <XMarkIcon className={styles.icon} />
+                        ) : (
+                            <Bars3Icon className={styles.icon} />
+                        )}
+                    </button>
+
+                    {/* Списък с линкове */}
+                    <ul
+                        className={`${styles.navList} ${
+                            isOpen ? styles.navOpen : ''
+                        }`}
+                    >
+                        {(menuItems || []).map((item, index) => (
+                            <li key={index} className={styles.navItem}>
+                                <a
+                                    href={item.url}
+                                    className={styles.navLink}
+                                >
+                                    {item.name}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </div>
