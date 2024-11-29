2ec1442ab0e40f3cec48d43a0d0b274518552a46
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-11-29 19:05:06 +0200

    Styled the hamburger menu to align with the mobile design specs.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,16 +14,12 @@ const Header = ({ logo, menuItems }) => {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-                {/* Лого */}
                 <div className={styles.logo}>
                     <a href="/">
                         <img src={logo} alt="Logo" className={styles.logoImg} />
                     </a>
                 </div>
-
-                {/* Навигационно меню */}
                 <nav className={styles.navContainer}>
-                    {/* Бутон за отваряне/затваряне */}
                     <button className={styles.menuToggle} onClick={onToggle}>
                         {isOpen ? (
                             <XMarkIcon className={styles.icon} />
@@ -31,12 +27,9 @@ const Header = ({ logo, menuItems }) => {
                             <Bars3Icon className={styles.icon} />
                         )}
                     </button>
-
-                    {/* Списък с линкове */}
                     <ul
-                        className={`${styles.navList} ${
-                            isOpen ? styles.navOpen : ''
-                        }`}
+                        className={`${styles.navList} ${isOpen ? styles.navOpen : ''
+                            }`}
                     >
                         {(menuItems || []).map((item, index) => (
                             <li key={index} className={styles.navItem}>
@@ -46,6 +39,7 @@ const Header = ({ logo, menuItems }) => {
                                 >
                                     {item.name}
                                 </a>
+                                <div className={styles.line}></div>
                             </li>
                         ))}
                     </ul>
